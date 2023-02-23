1e8cf4f37902e4fa9ef3377de07116720c497c02
Orlando Gil <77111097+orlando2400@users.noreply.github.com>
2023-02-23 12:52:08 -0500

    Se agrega proyecto a refactorizar

---
+++ src/com/example/chessgame/vista/ChessMain.java
@@ -0,0 +1,19 @@
+package com.example.chessgame.vista;
+import javax.swing.*;
+
+public class ChessMain{
+    // ----------------------------------------------------------
+    /**
+     * Creates the GUI for Chess.
+     *
+     * @param args
+     *            command line arguments, not used
+     */
+    public static void main( String[] args ){
+        JFrame frame = new JFrame( "YetAnotherChessGame 1.0" );
+        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        frame.getContentPane().add( new ChessPanel() );
+        frame.pack();
+        frame.setVisible( true );
+    }
+}
---
+++ src/com/example/chessgame/vista/ChessPanel.java
@@ -0,0 +1,83 @@
+package com.example.chessgame.vista;
+import com.example.chessgame.controlador.ChessGameLog;
+import com.example.chessgame.modelo.board.ChessGameBoard;
+import com.example.chessgame.modelo.game.ChessGameEngine;
+import com.example.chessgame.modelo.game.ChessGraveyard;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class ChessPanel extends JPanel{
+    private ChessMenuBar    menuBar;
+    private ChessGameBoard gameBoard;
+    private ChessGameLog gameLog;
+    private ChessGraveyard playerOneGraveyard;
+    private ChessGraveyard  playerTwoGraveyard;
+    private ChessGameEngine gameEngine;
+    // ----------------------------------------------------------
+    /**
+     * Create a new ChessPanel object.
+     */
+    public ChessPanel(){
+        this.setLayout( new BorderLayout() );
+        menuBar = new ChessMenuBar();
+        gameBoard = new ChessGameBoard();
+        gameLog = new ChessGameLog();
+        playerOneGraveyard = new ChessGraveyard( "Player 1's graveyard" );
+        playerTwoGraveyard = new ChessGraveyard( "Player 2's graveyard" );
+        this.add( menuBar, BorderLayout.NORTH );
+        this.add( gameBoard, BorderLayout.CENTER );
+        this.add( gameLog, BorderLayout.SOUTH );
+        this.add( playerOneGraveyard, BorderLayout.WEST );
+        this.add( playerTwoGraveyard, BorderLayout.EAST );
+        this.setPreferredSize( new Dimension( 800, 600 ) );
+        gameEngine = new ChessGameEngine( gameBoard ); // start the game
+    }
+    // ----------------------------------------------------------
+    /**
+     * Gets the logger object for use in other classes.
+     *
+     * @return Pieces.ChessGameLog the Pieces.ChessGameLog object
+     */
+    public ChessGameLog getGameLog(){
+        return gameLog;
+    }
+    // ----------------------------------------------------------
+    /**
+     * Gets the board object for use in other classes.
+     *
+     * @return ChessGameBoard the ChessGameBoard object
+     */
+    public ChessGameBoard getGameBoard(){
+        return gameBoard;
+    }
+    // ----------------------------------------------------------
+    /**
+     * Gets the game engine object for use in other classes
+     *
+     * @return ChessGameEngine the ChessGameEngine object
+     */
+    public ChessGameEngine getGameEngine(){
+        return gameEngine;
+    }
+    // ----------------------------------------------------------
+    /**
+     * Gets the appropriate graveyard object for use in other classes.
+     *
+     * @param whichPlayer
+     *            the number of the player (1 or 2)
+     * @return ChessGraveyard the graveyard requested
+     */
+    public ChessGraveyard getGraveyard( int whichPlayer ){
+        if ( whichPlayer == 1 ){
+            return playerOneGraveyard;
+        }
+        else if ( whichPlayer == 2 ){
+            return playerTwoGraveyard;
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
