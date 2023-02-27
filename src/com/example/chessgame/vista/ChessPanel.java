ddc368998daf50a2d78af7cc4679303ba3fb81a0
Daniel <josedanielgonzalesbroncano@gmail.com>
2023-02-27 09:59:53 -0500

    Refactorizacion y solucion de code shell

--- src/com/example/chessgame/vista/ChessPanel.java
+++ src/com/example/chessgame/vista/ChessPanel.java
@@ -13,7 +13,7 @@ public class ChessPanel extends JPanel{
     private ChessGameLog gameLog;
     private ChessGraveyard playerOneGraveyard;
     private ChessGraveyard  playerTwoGraveyard;
-    private ChessGameEngine gameEngine;
+    private transient ChessGameEngine gameEngine;
     // ----------------------------------------------------------
     /**
      * Create a new ChessPanel object.
